27b9b92fb0489f52b24c25b3d06d6599e258e62b
CrowsVeldt <zechy.mahler@gmail.com>
2023-12-03 18:22:58 -0700

    dev: add configuration for testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    //visualizer()
+  ],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/__tests__/setup.js",
+    globals: true,
+    watch: false,
+  },
+  // resolve: {
+  //   alias: {
+  //     "@": resolve(dirname(fileUrlToPath(import.meta.url)), "src"),
+  //   },
+  // },
+});
