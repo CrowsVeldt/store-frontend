94d8ed81ae24c9a4512d4ccb3db9ecc34ce3221c
CrowsVeldt <zechy.mahler@gmail.com>
2023-12-13 20:53:32 -0700

    ui: add editProductPage

---
+++ src/components/inputs/CategoryInput.jsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function CategoryInput() {
+  return (
+    <Box>
+      <Heading>Category</Heading>
+    </Box>
+  );
+}
--- src/pages/admin/EditProductPage.jsx
+++ src/pages/admin/EditProductPage.jsx
@@ -12,48 +12,57 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { convertToBase64 } from "../../utils/fileFuncs";
+import CategoryInput from "../../components/inputs/CategoryInput";
+
+/* 
+    Product Schema
+      product_name
+      product_description
+      product_price
+      product_image
+      catagories: [categoryid 1... 2... etc]
+  */
 
 export default function EditProduct() {
   const location = useLocation();
-  const Product = location.state;
+  const product = location.state;
   const nav = useNavigate();
   const axiosPrivateRoute = useAxiosPrivate();
 
-  //   const [values, setValues] = useState({
-  //     _id: user._id,
-  //     user_name: user?.user_name,
-  //     user_email: user?.user_email,
-  //     user_phone: user?.user_phone,
-  //     user_avatar: user?.user_avatar,
-  //     user_address: {
-  //       city: user?.user_address?.city || "",
-  //       street: user?.user_address?.street || "",
-  //       building: user?.user_address?.building || "",
-  //       apartment: user?.user_address?.apartment || "",
-  //     },
-  //   });
+  const [values, setValues] = useState({
+    _id: product._id,
+    product_name: product?.product_name,
+    product_description: product?.product_description,
+    product_price: product?.product_price,
+    product_image: product?.product_image,
+    //     categories: [
+    //     categories(?)
+    //     ],
+  });
 
-  //   const handleSaveButton = async () => {
-  //     try {
-  //       const response = await axiosPrivateRoute.patch(
-  //         `/admin/${user._id}/edit/user`,
-  //         values
-  //       );
+  const handleSaveButton = async () => {
+    try {
+      const response = await axiosPrivateRoute.patch(
+        `/admin/${product._id}/edit/product`,
+        values
+      );
 
-  //       setValues((prevValues) => {
-  //         return { ...prevValues, ...response?.data?.user };
-  //       });
+      console.log(response);
 
-  //       nav("/admin/edit/user", {
-  //         state: response.data.user,
-  //         replace: true,
-  //       });
+      setValues((prevValues) => {
+        return { ...prevValues, ...response?.data?.product };
+      });
 
-  //       toast.success(`Updated ${values?.user_name}'s account info`);
-  //     } catch (error) {
-  //       toast.error(error?.response?.data?.message);
-  //     }
-  //   };
+      nav("/admin/edit/product", {
+        state: response.data.product,
+        replace: true,
+      });
+
+      toast.success(`Updated ${values?.product_name} details`);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   //   const handleNestedChange = (e) => {
   //     setValues((prevValues) => ({
@@ -65,143 +74,88 @@ export default function EditProduct() {
   //     }));
   //   };
 
-  //   const handleFileUpload = async (e) => {
-  //     const file = e?.target?.files[0];
-  //     const base64image = await convertToBase64(file);
-  //     setValues((prevValues) => ({ ...prevValues, user_avatar: base64image }));
-  //   };
+  const handleFileUpload = async (e) => {
+    const file = e?.target?.files[0];
+    const base64image = await convertToBase64(file);
+    setValues((prevValues) => ({ ...prevValues, product_image: base64image }));
+  };
 
-  //   const handleChange = (e) => {
-  //     setValues((prevValues) => ({
-  //       ...prevValues,
-  //       [e?.target?.name]: e?.target?.value,
-  //     }));
-  //   };
+  const handleChange = (e) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [e?.target?.name]: e?.target?.value,
+    }));
+  };
 
-  //   return (
-  //     <Box minH="65vh" py={10} px={4}>
-  //       <Avatar name={values?.user_name} src={values?.user_avatar} size={"xl"} />
-  //       <Input
-  //         name="user_avatar"
-  //         type="file"
-  //         onChange={handleFileUpload}
-  //         accept=".jpeg, .png, .jpg"
-  //       />
-  //       <Stack spacing={2}>
-  //         <Heading as="h3" fontSize="lg">
-  //           Details
-  //         </Heading>
-  //         <Text fontSize="md">
-  //           <Text as="span" fontWeight="bold">
-  //             Name:{" "}
-  //           </Text>
-  //           <Input
-  //             name="user_name"
-  //             type="text"
-  //             placeholder={`${values.user_name ? values.user_name : "Name"}`}
-  //             value={values?.user_name || ""}
-  //             onChange={handleChange}
-  //           />
-  //         </Text>
-  //         <Text fontSize="md">
-  //           <Text as="span" fontWeight="bold">
-  //             Email:{" "}
-  //           </Text>
-  //           <Input
-  //             name="user_email"
-  //             type="email"
-  //             placeholder={`${
-  //               values.user_email ? values.user_email : "Email Address"
-  //             }`}
-  //             value={values?.user_email || ""}
-  //             onChange={handleChange}
-  //           />
-  //         </Text>
-  //         <Text fontSize="md">
-  //           <Text as="span" fontWeight="bold">
-  //             Phone:{" "}
-  //           </Text>
-  //           <Input
-  //             name="user_phone"
-  //             type="text"
-  //             placeholder={`${
-  //               values.user_phone ? values.user_phone : "Phone Number"
-  //             }`}
-  //             value={values?.user_phone || ""}
-  //             onChange={handleChange}
-  //           />
-  //         </Text>
-  //         <Heading as="h3" fontSize="lg">
-  //           Address
-  //         </Heading>
-  //         <Text>
-  //           <Text as="span" fontWeight="bold">
-  //             City:{" "}
-  //           </Text>
-  //           <Input
-  //             name="city"
-  //             type="text"
-  //             placeholder={`${
-  //               values.user_address.city
-  //                 ? values.user_address.city
-  //                 : "Town/City Name"
-  //             }`}
-  //             value={values.user_address?.city || ""}
-  //             onChange={handleNestedChange}
-  //           />
-  //         </Text>
-  //         <Text>
-  //           <Text as="span" fontWeight="bold">
-  //             Street:{" "}
-  //           </Text>
-  //           <Input
-  //             name="street"
-  //             type="text"
-  //             placeholder={`${
-  //               values.user_address.street
-  //                 ? values.user_address.street
-  //                 : "Street Name"
-  //             }`}
-  //             value={values.user_address?.street || ""}
-  //             onChange={handleNestedChange}
-  //           />
-  //         </Text>
-  //         <Text fontSize="md">
-  //           <Text as="span" fontWeight="bold">
-  //             Building:{" "}
-  //           </Text>
-  //           <Input
-  //             name="building"
-  //             type="text"
-  //             placeholder={`${
-  //               values.user_address.building
-  //                 ? values.user_address.building
-  //                 : "Building Number"
-  //             }`}
-  //             value={values?.user_address?.building || ""}
-  //             onChange={handleNestedChange}
-  //           />
-  //         </Text>
-  //         <Text fontSize="md">
-  //           <Text as="span" fontWeight="bold">
-  //             Apartment:{" "}
-  //           </Text>
-  //           <Input
-  //             name="apartment"
-  //             type="text"
-  //             placeholder={`${
-  //               values.user_address.apartment
-  //                 ? values.user_address.apartment
-  //                 : "Apartment Number"
-  //             }`}
-  //             value={values?.user_address?.apartment || ""}
-  //             onChange={handleNestedChange}
-  //           />
-  //         </Text>
-  //       </Stack>
-  //       <Button mt={4} colorScheme="teal" onClick={handleSaveButton}>
-  //         Save
-  //       </Button>
-  //     </Box>
-  //   );
+  return (
+    <Box minH="65vh" py={10} px={4}>
+      <Avatar
+        name={values?.product_name}
+        src={values?.product_image}
+        size={"xl"}
+      />
+      <Input
+        name="product_image"
+        type="file"
+        onChange={handleFileUpload}
+        accept=".jpeg, .png, .jpg"
+      />
+      <Stack spacing={2}>
+        <Heading as="h3" fontSize="lg">
+          Details
+        </Heading>
+        <Text fontSize="md">
+          <Text as="span" fontWeight="bold">
+            Name:{" "}
+          </Text>
+          <Input
+            name="product_name"
+            type="text"
+            placeholder={`${
+              values.product_name ? values.product_name : "Name"
+            }`}
+            value={values?.product_name || ""}
+            onChange={handleChange}
+          />
+        </Text>
+        <Text fontSize="md">
+          <Text as="span" fontWeight="bold">
+            Description:{" "}
+          </Text>
+          <Input
+            name="product_description"
+            type="text"
+            placeholder={`${
+              values.product_description
+                ? values.product_description
+                : "Description"
+            }`}
+            value={values?.product_description || ""}
+            onChange={handleChange}
+          />
+        </Text>
+        <Text fontSize="md">
+          <Text as="span" fontWeight="bold">
+            Price:{" "}
+          </Text>
+          <Input
+            name="product_price"
+            type="text"
+            placeholder={`${
+              values.product_price ? values.product_price : "Price"
+            }`}
+            value={values?.product_price || ""}
+            onChange={handleChange}
+          />
+        </Text>
+        {/* categories input */}
+        {product.categories.map((category, index) => {
+          <CategoryInput state={category._id} key={index} />;
+        })}
+      </Stack>
+      <Button mt={4} colorScheme="teal" onClick={handleSaveButton}>
+        Save
+      </Button>
+    </Box>
+  );
 }
