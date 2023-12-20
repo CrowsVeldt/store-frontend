347f971056c556527b690534f12915ba6246a8b9
CrowsVeldt <zechy.mahler@gmail.com>
2023-12-19 17:23:00 -0700

    dev: add uniqueObjectArray to utilFuncs

--- src/utils/utilFuncs.js
+++ src/utils/utilFuncs.js
@@ -3,3 +3,12 @@ export const formatPhone = (number) => {
   const endDigits = number.slice(-4);
   return `(${phoneSegments[0]}) ${phoneSegments[1]}-${endDigits}`;
 };
+
+export const uniqueObjectArray = (arrayOfObjects) =>
+  arrayOfObjects.filter(
+    (object, index) =>
+      index ===
+      arrayOfObjects.findIndex(
+        (obj) => JSON.stringify(obj) === JSON.stringify(object)
+      )
+  );
