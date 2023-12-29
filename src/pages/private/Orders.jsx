1cb98ec194a1f6713630f3315b10bba5e36065b5
CrowsVeldt <zechy.mahler@gmail.com>
2023-12-29 09:46:35 -0700

    ui: add loadingCircle to user orders page

--- src/pages/private/Orders.jsx
+++ src/pages/private/Orders.jsx
@@ -3,29 +3,38 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import OrdersList from "../../components/orders/OrdersList";
+import LoadingCircle from "../../components/info/LoadingCircle";
 
 export default function UserOrders() {
   const { user } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
   const [orders, setOrders] = useState([]);
   const usePrivateRoutes = useAxiosPrivate();
 
   useEffect(() => {
     (async () => {
-      const userOrders = await usePrivateRoutes.get(
-        `/orders/${user?.user?._id}/orders`
-      );
-      setOrders(userOrders.data.orders);
+      try {
+        const userOrders = await usePrivateRoutes.get(`/orders/${user?.user?._id}/orders`);
+        setOrders(userOrders.data.orders);
+        setIsLoading(false)
+      } catch (err) {
+        setIsLoading(false)
+      }
     })();
   }, []);
 
   return (
     <Box>
       <Heading>User Orders</Heading>
-      <UnorderedList>
-        {(orders.length > 0 && (
-          <OrdersList orders={orders} editable={false} />
-        )) || <Text>No orders found</Text>}
-      </UnorderedList>
+      {!isLoading ? (
+        <UnorderedList>
+          {(orders.length > 0 && (
+            <OrdersList orders={orders} editable={false} />
+          )) || <Text>No orders found</Text>}
+        </UnorderedList>
+      ) : (
+        <LoadingCircle />
+      )}
     </Box>
   );
 }
